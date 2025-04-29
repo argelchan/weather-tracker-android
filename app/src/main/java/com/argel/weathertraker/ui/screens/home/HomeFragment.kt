e0ba3dc485a6e257ab5db878a7ad2baab60c9cc1
Didier <didier.chan@ingenierosmafur.com>
2025-04-29 16:39:21 -0600

    - Added my location, zoom and alert dialog

--- app/src/main/java/com/argel/weathertraker/ui/screens/home/HomeFragment.kt
+++ app/src/main/java/com/argel/weathertraker/ui/screens/home/HomeFragment.kt
@@ -1,15 +1,23 @@
 package com.argel.weathertraker.ui.screens.home
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.argel.weathertraker.R
 import com.argel.weathertraker.core.presentation.BaseFragment
 import com.argel.weathertraker.databinding.FragmentHomeBinding
+import com.argel.weathertraker.ui.customs.informativeDialog.InformativeDialog
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.WithFragmentBindings
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -20,18 +28,82 @@ import kotlinx.coroutines.DelicateCoroutinesApi
 class HomeFragment : BaseFragment(R.layout.fragment_home) {
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var googleMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
     override fun setBinding(view: View) {
         binding = FragmentHomeBinding.bind(view)
         binding.apply {
             lifecycleOwner = viewLifecycleOwner
         }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+    }
+
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                enableMyLocationAndZoom()
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                enableMyLocation()
+            } else -> {
+                InformativeDialog(
+                    R.string.notice,
+                    R.string.location_permission_denied,
+                    negativeEnabled = false,
+                    positiveCallback = {}
+                ).show(childFragmentManager, "InformativeDialog")
+            }
+        }
+    }
+
+
+    private fun checkLocationPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            enableMyLocationAndZoom()
+        } else {
+            requestLocationPermissions()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocationAndZoom() {
+        googleMap.isMyLocationEnabled = true
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            location?.let {
+                val currentLatLng = LatLng(it.latitude, it.longitude)
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+        googleMap.isMyLocationEnabled = true
+    }
+
+    private fun requestLocationPermissions() {
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
     }
 
     private val callback = OnMapReadyCallback { googleMap ->
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        this.googleMap = googleMap
+        checkLocationPermissions()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
